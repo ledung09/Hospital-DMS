49be7fc0465e5ef008c71134cf63ca749f03fc0a
Le Dung <dp09.it@gmail.com>
2023-12-13 22:31:02 +0700

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 "use client"
 
-import { useRouter } from 'next/navigation'
+import { redirect } from 'next/navigation'
 
 export default function Page() {
-  // const router = useRouter();
-  // router.push('/patients/find')
+  redirect('/patients/find')
   return (
     <></>
   )
